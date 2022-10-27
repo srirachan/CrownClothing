3f5fc66957a8359885bab7323ea8de3cd9df26f5
Srirachan <srirachan0357@gmail.com>
2022-10-27 15:30:52 +0530

    categories updated

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import CategoryItem from "./components/category-item/category-item";
+import "./category.scss";
+
+const App = () => <CategoryItem />;
+
+export default App;
---
+++ src/components/category-item/category-item.js
@@ -0,0 +1,52 @@
+import "./category-item.scss";
+
+const CategoryItem = () => {
+  const category = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return (
+    <div class='categories-container'>
+      {category.map((eachItem) => {
+        const { id, title, imageUrl } = eachItem;
+        return (
+          <div key={id} class='category-container'>
+            <div
+              class='background-image'
+              style={{ backgroundImage: `url(${imageUrl})` }}
+            ></div>
+            <div class='category-body-container'>
+              <h2>{title}</h2>
+              <p>SHOP NOW</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CategoryItem;
